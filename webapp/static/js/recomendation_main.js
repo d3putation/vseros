68d431b845c12078a30f570229c5feaa8e093517
blackUser-hub <kostylevtimofej354@gmail.com>
2024-09-29 04:19:52 +0500

    second page and js interactive

---
+++ webapp/static/js/recomendation_main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.video').on('click', function(){
+        console.log($(this));
+        localStorage.setItem("at", $(this).attr('id'));
+        document.location.href = "../templates/single_video.html";
+    });
+});
