5e87ea715f5de77818efc4e77466cfa068fc4dbc
d3putation <94278254+d3putation@users.noreply.github.com>
2024-09-29 00:51:52 +0500

    aaaaaaa

---
+++ webapp/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, session, render_template, redirect
+from flask_session import Session
+import time
+import requests
+
+
+app = Flask(__name__)
+app.secret_key = 'bebra'
+app.debug = True
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app=app)
+
+def get_latest_id():
+    return str(time.time())
+
+def add_user(id):
+    requests.post("http://backend:8000/users/new_user", params=id)
+
+@app.route("/")
+def startpage():
+    if "id" not in session:
+        id = get_latest_id()
+        print("got")
+        add_user(id)
+        session["id"] = id
+
+    names = []
+
+    descriptions = []
+    categories = []
+    return render_template("recomendation.html", names = names, descriptions = descriptions, categories = categories)
+
+@app.route("/video/{id}")
+def video(id):
+    name = ""
+    description = ""
+    category = ""
+    return render_template("single_video.html", name = name, description = description, category = category)
+
+if __name__ == "__main__":
+    app.run(host="webapp", port=5555)
